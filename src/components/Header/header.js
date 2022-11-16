1d5181ca23043260c2e01939cf45860e01cedbc4
Tarang Verma <tarangverma004@gmail.com>
2022-11-16 20:33:16 +0530

    Navbar done
    Signed-off-by: Tarang Verma <tarangverma004@gmail.com>

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -7,6 +7,33 @@ function header() {
         <Logo>
             <img src='/images/logo.svg'></img>
         </Logo>
+        <NavMenu>
+          <a href='/home' >
+          <img src='/images/home-icon.svg' alt='no-img'></img>  
+          <span>HOME</span>
+          </a>  
+          <a>
+          <img src='/images/search-icon.svg' alt='no-imgg'></img>  
+          <span>SEARCH</span>
+          </a>  
+          <a href='/home' >
+          <img src='/images/watchlist-icon.svg' alt='no-imge'></img>  
+          <span>WATCHLIST</span>
+          </a>  
+          <a>
+          <img src='/images/original-icon.svg' alt='no-imga'></img>  
+          <span>ORIGINALS</span>
+          </a>  
+          <a>
+          <img src='/images/movie-icon.svg' alt='no-imgc'></img>  
+          <span>MOVIE</span>
+          </a>
+          <a>
+          <img src='/images/series-icon.svg' alt='no-imgv'></img>  
+          <span>SERIES</span>
+          </a>    
+        </NavMenu>
+        <Login>LOGIN</Login>
     </Nav>
     );
 };
@@ -38,6 +65,85 @@ img {
     display: block;
     width: 100%;
 }
+`;
+
+const NavMenu = styled.div`
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  height: 100%;
+  justify-content: flex-end;
+  margin: 0;
+  padding: 0;
+  position: relative;
+  margin-right: auto;
+  margin-left: 25px; 
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+
+  img {
+    height: 20px;
+    min-height: 20px;
+    width: 20px;
+  }
+
+  span {
+    color: rgb(249, 249 , 249);
+    font-size: 13px;
+    letter-spacing: 1.2px;
+    line-height: 1.08;
+    padding: 2px 0;
+    wight-space: nowrap;
+    position: relative;
+
+  &:before {
+      background-color: rgb(249 , 249 , 249);
+      border-radius: 0px 0px 4px 4px;
+      bottom: -6px;
+      left: 0px;
+      content: "";
+      height: 2px;
+      opacity: 0;
+      position: absolute;
+      right: 0px;
+      transform-origin: left center;
+      transform: scaleX(0);
+      transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+      visibility: hidden;
+      width: auto;
+  }
+}
+
+&:hover {
+  span:before {
+    transform: scaleX(1);
+    visibility: visible;
+    opacity: 1 !important;
+  }
+}
+}
+
+   @media (max-width: 768px) {
+     display: none;
+   }
+`;
+
+const Login = styled.a`
+ background-color: rgba(0, 0 , 0 , 0.6);
+ padding: 8px 16px;
+ text-transform: uppercase;
+ letter-spacing: 1.5px;
+ border: 1px solid #f9f9f9;
+ border-radius: 4px;
+ transition: all 0.2s ease 0s;
+
+ &:hover {
+  background-color: #f9f9f9;
+  color: #000;
+ }
 `;
 
 export default header;
